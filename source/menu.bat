8b95bbfb45072a5df4db3b39769d62b9eaeeaaf1
Andrii <occomputrr@gmail.com>
2023-08-15 16:50:34 +0200

    Update menu.bat

--- source/menu.bat
+++ source/menu.bat
@@ -1,1 +1,11 @@
-
+@echo off
+cls
+taskkill /F /IM explorer.exe
+color A
+echo welcome
+echo menu
+echo 1 CMD
+echo 2 EXIT
+set /p choose="Choose:"
+if %choose% == 1 start cmd.exe
+if %choose% == 2 C:\Windows\explorer.exe
