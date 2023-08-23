bea37492c8689a68936a7e9d358e2b585a2144bc
Andrii <occomputrr@gmail.com>
2023-08-23 19:22:52 +0200

    Update menu.bat

--- source/menu.bat
+++ source/menu.bat
@@ -1,4 +1,6 @@
+:MENU
 @echo off
+title TRASHCMDOS MENU.BAT
 cls
 taskkill /F /IM explorer.exe
 color A
@@ -7,5 +9,14 @@ echo menu
 echo 1 CMD
 echo 2 EXIT
 set /p choose="Choose:"
-if %choose% == 1 start cmd.exe
-if %choose% == 2 C:\Windows\explorer.exe
+if %choose% == 1 GOTO :CMD
+if %choose% == 2 GOTO :EXIT
+:EXIT
+title CLOSE CMD WINDOW BY YOURSELF!
+echo something is wrong. close CMD window by yourself
+C:\Windows\explorer.exe
+:CMD
+echo you will start in SOURCE FOLDER
+start cmd.exe
+pause
+GOTO :MENU
